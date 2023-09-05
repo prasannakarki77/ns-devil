4359edb652b0324ec077c5371a2a477c356b36e8
prasannakarki77 <prasanna.karki77@gmail.com>
2023-09-05 20:34:23 +0545

    removed clientOnly wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import ClientOnly from "@/components/ClientOnly";
 import { SignInForm } from "@/components/forms/SignInForm";
 import getCurrentUser from "./actions/getCurrentUser";
 import { StudentTable } from "@/components/academics/StudentTable";
 
 export default async function Home() {
   const currentUser = await getCurrentUser();
-
   return (
     <main className=" container min-h-screen p-10 ">
       {!currentUser ? (
-        <ClientOnly>
-          <div className="mt-[10vh] flex flex-col items-center">
-            {" "}
-            <SignInForm />
-          </div>
-        </ClientOnly>
+        <div className="mt-[10vh] flex flex-col items-center">
+          <SignInForm />
+        </div>
       ) : (
         <div className="">
           <h1 className="text-xl font-bold mb-4 ">Students Table</h1>
